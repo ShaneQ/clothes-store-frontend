653129a6060f4df2dead20ef860f49775eb68972
shanequaid-toast <shane.quaid@toasttab.com>
2021-11-14 20:22:25 +0000

    Size filtering

--- src/main/resources/src/app/product-list/product-list.component.ts
+++ src/main/resources/src/app/product-list/product-list.component.ts
@@ -3,6 +3,8 @@ import {AppService} from '../app.service';
 import {ProductService} from '../product.service';
 import {Observable} from 'rxjs';
 import {Product} from '../model/product';
+import {SearchProductsService} from "../search-products.service";
+import {Size} from "../model/size";
 
 @Component({
   selector: 'app-product-list',
@@ -11,24 +13,60 @@ import {Product} from '../model/product';
 })
 export class ProductListComponent implements OnInit {
   public products$: Observable<Product[]>;
+  public allProducts$: Observable<Product[]>;
+  public filteredProducts: Product[] = [];
+
   productCount: number;
 
   @Input()
   displayAmount: number;
 
-  constructor(private _service: AppService, private _dataService: ProductService) {
+  constructor(private _service: AppService, private _dataService: ProductService, private _searchService: SearchProductsService) {
     this.productCount = 0;
-    if (!this.displayAmount){
+    if (!this.displayAmount) {
       this.displayAmount = 50;
     }
   }
 
   getProducts() {
     this.products$ = this._dataService.loadProducts();
+    this.allProducts$ = this.products$
   }
 
   ngOnInit(): void {
     this.getProducts();
+    this._searchService.sizeFilterClickedEvent
+    .subscribe((data: string) => {
+      if(data.length > 0){
+        let b = data.split(',').map(function(item) {
+          return parseInt(item, 10);
+        });
+        this.filterProducts(b)
+      }else{
+        this.products$ = this.allProducts$
+      }
+    });
+  }
+
+
+  filterProducts(sizes: number[]) {
+    this.products$ = this.allProducts$.map(products => products.filter(product => this.filterSizes(product.sizes, sizes)))
+  }
+
+  filterSizes(sizes: Size[], sizeArr: Number[]):boolean{
+    let sizeFound = sizes.filter(value => sizeArr.includes(value.id));
+    if(sizeArr.length > 0 && sizeFound.length > 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  loopThroughArray(size: Number, sizeArr: Number[]){
+    console.log(size)
+    console.log(sizeArr)
+    return sizeArr.includes(size)
+
   }
 
   onProductSelected(product: Product) {
--- src/main/resources/src/app/shop-filter/shop-filter.component.ts
+++ src/main/resources/src/app/shop-filter/shop-filter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {ProductService} from "../product.service";
+import {SearchProductsService} from "../search-products.service";
 
 @Component({
   selector: 'app-shop-filter',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop-filter.component.scss']
 })
 export class ShopFilterComponent implements OnInit {
-
-  constructor() { }
+  searchForm: FormGroup
+  Sizes: Array<any> = [
+    { name: 'XS', value: '2' },
+    { name: 'S', value: '3' },
+    { name: 'M', value: '4' },
+    { name: 'L', value: '5' },
+    { name: 'XL', value: '6' },
+    { name: 'One Size', value: '1' }
+  ];
+  constructor(private fb: FormBuilder, private _searchService: SearchProductsService) { }
 
   ngOnInit(): void {
+    this.searchForm = this.fb.group({
+      sizes: this.fb.array([])
+    })
   }
 
+  filterProducts(){
+    let sizesArr = this.searchForm.get('sizes').value.toString()
+    this._searchService.sizeClicked(sizesArr);
+  }
+
+  onCheckboxChange(e) {
+    const checkArray: FormArray = this.searchForm.get('sizes') as FormArray;
+
+    if (e.target.checked) {
+      checkArray.push(new FormControl(e.target.value));
+    } else {
+      let i: number = 0;
+      checkArray.controls.forEach((item: FormControl) => {
+        if (item.value == e.target.value) {
+          checkArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+    this.filterProducts()
+  }
 }
