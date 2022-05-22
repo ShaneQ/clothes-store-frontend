3dadaac0f2d01e9a4adaaf43aca3ceddcd819f96
shanequaid-toast <shane.quaid@toasttab.com>
2022-05-22 21:04:48 +0100

    Fixed howitworks page and content

--- src/app/module-admin/services/script.service.ts
+++ src/app/module-admin/services/script.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ScriptStore } from './admin/store/script.store';
+import {ScriptStore} from "../../store/script.store";
 
 declare var document: any;
 
