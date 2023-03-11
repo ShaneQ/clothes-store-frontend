5d4d5f76c1e76b245f3e1693fa1fa85d2c424319
shanequaid-toast <shane.quaid@toasttab.com>
2023-03-11 20:18:46 +0000

    SCCV2-45 - Added new shop carrousel

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -6,7 +6,7 @@ import {KeycloakService} from 'keycloak-angular';
 import {ProductService} from '../services/product.service';
 import {environment} from '../../environments/environment';
 import SwiperCore, {Navigation, Pagination, Scrollbar, SwiperOptions} from "swiper";
-
+import {stuff} from '../model/shop';
 SwiperCore.use([Navigation, Pagination, Scrollbar]);
 
 @Component({
@@ -21,6 +21,7 @@ export class ContentComponent implements OnInit {
     pagination: {clickable: true},
     scrollbar: {draggable: true},
     spaceBetween: 25,
+    speed: 1000,
     breakpoints: {
       320: {
         slidesPerView: 1
@@ -30,9 +31,6 @@ export class ContentComponent implements OnInit {
       },
       990: {
         slidesPerView: 4
-      },
-      1200: {
-        slidesPerView: 5
       }
     }
   };
@@ -41,6 +39,7 @@ export class ContentComponent implements OnInit {
 
   private baseUrl: string = environment.baseUrl;
   private location: string = window.location.href;
+  public productsNew: string[] = this.shuffleArray(stuff);
 
   constructor(
     private _script: ScriptService,
@@ -66,5 +65,19 @@ export class ContentComponent implements OnInit {
       newLocation = this.baseUrl + '/browser';
     }
     this._keycloak.register({redirectUri: newLocation});
+  }
+
+  shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+
+      // Generate random number
+      var j = Math.floor(Math.random() * (i + 1));
+
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+
+    return array;
   }
 }
